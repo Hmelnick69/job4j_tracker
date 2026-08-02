4593cc85ae5002b9916cd32ce8566247985ae24d
TimofeyHarkunov <tharkunov@gmail.com>
2026-08-02 21:26:12 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/test/java/ru/job4j/tracker/checkstyle/Broken.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker.checkstyle;
+
+public class Broken {
+    private final int sizeOfEmpty = 10;
+    public String surname;
+    public String newValue;
+
+    void echo() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b) {
+            String surname;
+        }
+
+        Broken() {
+
+        }
+    }
