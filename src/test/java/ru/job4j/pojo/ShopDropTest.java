6d4a2153be99624dffe452add149f1a7e586f713
TimofeyHarkunov <tharkunov@gmail.com>
2026-08-28 20:18:43 +0300

    3.2. Удаление моделей из массива. [#310016 #566622]

--- src/test/java/ru/job4j/pojo/ShopDropTest.java
+++ src/test/java/ru/job4j/pojo/ShopDropTest.java
@@ -3,57 +3,78 @@ package ru.job4j.pojo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @Execution(ExecutionMode.CONCURRENT)
 
 public class ShopDropTest {
     @Test
-    public void whenDropFirst() {
-        Product[] products = new Product[2];
-        products[0] = new Product("Milk", 10);
-        products[1] = new Product("Bread", 4);
-        Product[] result = ShopDrop.delete(products, 0);
-        assertThat(result[0].getName()).isEqualTo("Bread");
-        assertThat(result[1]).isNull();
-    }
-
-    @Test
     public void whenDropLast() {
-        Product[] products = new Product[2];
-        products[0] = new Product("Milk", 10);
-        products[1] = new Product("Bread", 4);
+        Product[] products = {
+                new Product("Milk", 10),
+                new Product("Bread", 4)
+        };
         Product[] result = ShopDrop.delete(products, 1);
-        assertThat(result[0].getName()).isEqualTo("Milk");
-        assertThat(result[1]).isNull();
+        Product[] expected = {
+                new Product("Milk", 10),
+                null
+        };
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void whenArrayLengthIs4AndDeleteElementWithIndex1() {
-        Product[] products = new Product[4];
-        products[0] = new Product("Bread", 4);
-        products[1] = new Product("Egg", 10);
-        products[2] = new Product("Milk", 2);
-        products[3] = new Product("Fish", 3);
-        int index = 1;
-        Product[] result = ShopDrop.delete(products, index);
-        assertThat(result[index].getName()).isEqualTo("Milk");
-        assertThat(result[2].getName()).isEqualTo("Fish");
-        assertThat(result[products.length - 1]).isNull();
+        Product[] products = {
+                new Product("Bread", 4),
+                new Product("Egg", 10),
+                new Product("Milk", 2),
+                new Product("Fish", 3)
+        };
+        Product[] result = ShopDrop.delete(products, 1);
+        Product[] expected = {
+                new Product("Bread", 4),
+                new Product("Milk", 2),
+                new Product("Fish", 3),
+                null
+        };
+
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void whenArrayLengthIs5AndDeleteElementWithIndex2() {
-        Product[] products = new Product[5];
-        products[0] = new Product("Bread", 4);
-        products[1] = new Product("Egg", 10);
-        products[2] = new Product("Milk", 2);
-        products[3] = new Product("Fish", 3);
-        products[4] = new Product("Fruit", 8);
-        int index = 2;
-        Product[] result = ShopDrop.delete(products, index);
-        assertThat(result[1].getName()).isEqualTo("Egg");
-        assertThat(result[index].getName()).isEqualTo("Fish");
-        assertThat(result[3].getName()).isEqualTo("Fruit");
-        assertThat(result[products.length - 1]).isNull();
+        Product[] products = {
+                new Product("Bread", 4),
+                new Product("Egg", 10),
+                new Product("Milk", 2),
+                new Product("Fish", 3),
+                new Product("Fruit", 8)
+        };
+        Product[] result = ShopDrop.delete(products, 2);
+        Product[] expected = {
+                new Product("Bread", 4),
+                new Product("Egg", 10),
+                new Product("Fish", 3),
+                new Product("Fruit", 8),
+                null
+        };
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenArrayLengthIs3AndLastIsNullDeleteFirst() {
+        Product[] products = {
+                new Product("Bread", 4),
+                new Product("Egg", 10),
+                null
+        };
+        Product[] result = ShopDrop.delete(products, 0);
+        Product[] expected = {
+                new Product("Egg", 10),
+                null,
+                null
+        };
+        assertThat(result).isEqualTo(expected);
     }
 }
